24c98a4abe4cd7bcd9801e45cd79e33195d7ec41
Chen Lu <chenlu22z@ict.ac.cn>
2022-10-28 13:02:37 +0800

    add auto-testing script

--- test/run-test.sh
+++ test/run-test.sh
@@ -1,3 +1,20 @@
 mkdir -p build
-gcc testlib.c test$1.c -o build/test$1
-./build/test$1
+for i in {0..24}
+do
+   idx="$i"
+   if [ $i -lt 10 ]; then
+      idx="0$i"
+   fi
+   gcc testlib.c test$idx.c -o build/test$idx
+   file_ref=./build/testout$idx-ref.txt
+   file_out=./build/testout$idx.txt
+   ./build/test$idx &> $file_ref
+   ../ast-interpreter/build/ast-interpreter "`cat test$idx.c`" &> $file_out
+
+   if [ "$(diff $file_ref $file_out)" ];
+   then
+      echo "test$idx fail...."
+   else
+      echo "test$idx pass!!"
+   fi
+done
